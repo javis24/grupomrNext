d84ca62bca1125ec419896cec2a04fb6638ac87e
Javier Mtz <luisjaviermtzb@gmail.com>
2024-09-02 15:54:11 -0600

    primer commit

--- src/config/Database.js
+++ src/config/Database.js
@@ -2,17 +2,7 @@ import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
   host: process.env.MYSQL_HOST,
-  port: 3306, 
   dialect: 'mysql',
-  logging: false, // Puedes habilitar el logging para ver consultas SQL en la consola
 });
-
-sequelize.authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
 
 export default sequelize;
