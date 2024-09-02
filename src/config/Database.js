953ae65b81f1e82f47fc7740ecc50c7efda40913
Javier Mtz <luisjaviermtzb@gmail.com>
2024-09-02 11:51:30 -0600

    primer commit

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from 'sequelize';
 
-const db = new Sequelize('gmr_auth', 'gmr_authJavis', 'Sfdk#2011', {
-    host: 'localhost',
-    dialect: 'mysql'
+const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
 });
 
-export default db;
+export default sequelize;
