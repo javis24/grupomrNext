8c66edda9b38a9106c2c607afec77cb0b5df52fa
Javier Mtz <luisjaviermtzb@gmail.com>
2024-09-02 16:15:24 -0600

    primer commit

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,8 +1,13 @@
 import { Sequelize } from 'sequelize';
 
-const sequelize = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-  host: process.env.MYSQL_HOST,
-  dialect: 'mysql',
-});
+const sequelize = new Sequelize(
+  process.env.MYSQLDATABASE,   
+  process.env.MYSQLUSER,       
+  process.env.MYSQLPASSWORD,   
+  {
+    host: process.env.MYSQLHOST, 
+    dialect: 'mysql',
+  }
+);
 
 export default sequelize;
