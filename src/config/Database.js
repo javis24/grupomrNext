4d561f58ae88410ef75d7d5bc0e5d7be07f165bf
Javier Mtz <luisjaviermtzb@gmail.com>
2024-09-02 16:47:44 -0600

    update config database

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,23 +1,14 @@
 import { Sequelize } from 'sequelize';
 
 const sequelize = new Sequelize(
-  process.env.MYSQLDATABASE,
-  process.env.MYSQLUSER,
-  process.env.MYSQLPASSWORD,
+  process.env.MYSQLDATABASE,   
+  process.env.MYSQLUSER,       
+  process.env.MYSQLPASSWORD,    
   {
-    host: process.env.MYSQLHOST,
-    dialect: 'mysql',
-    logging: console.log,
+    host: process.env.MYSQLHOST, 
+    dialect: 'mysql',             
+    logging: console.log,         
   }
 );
 
-async function testConnection() {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
-
-testConnection();
+export default sequelize;
