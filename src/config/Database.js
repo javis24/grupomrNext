93f212012e760e2e1dfdf3cfb01af10f9b4c2328
Javier Mtz <luisjaviermtzb@gmail.com>
2024-09-02 19:04:37 -0600

    config/env

--- src/config/Database.js
+++ src/config/Database.js
@@ -1,19 +1,10 @@
 import { Sequelize } from 'sequelize';
-import dotenv from 'dotenv';
 
-dotenv.config(); 
+const db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
+    host: process.env.MYSQL_HOST,
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),  
+    logging: console.log,
+});
 
-let db;
-
-const getDatabaseConnection = () => {
-    if (!db) {
-        db = new Sequelize(process.env.MYSQL_DATABASE, process.env.MYSQL_USER, process.env.MYSQL_PASSWORD, {
-            host: process.env.MYSQL_HOST,
-            dialect: 'mysql',
-            logging: console.log,  // Mantén el logging activado o desactivado según lo necesites
-        });
-    }
-    return db;
-};
-
-export default getDatabaseConnection;
+export default db;
